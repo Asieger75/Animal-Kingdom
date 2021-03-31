c9ab82298bde0cc1761765d2e227459664008e6b
Maddison Taitano <maddisontaitano@gmail.com>
2021-03-30 20:34:06 -0400

    blah

--- Solved/client/src/App.js
+++ Solved/client/src/App.js
@@ -1,28 +1,12 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Books from "./pages/Books";
-import Detail from "./pages/Detail";
-import NoMatch from "./pages/NoMatch";
-import Nav from "./components/Nav";
-
+import React from "react"
+import './App.css';
+import Continents from'./components/Continents/Continents'
 function App() {
   return (
-    <Router>
-      <div>
-        <Nav />
-        <Switch>
-          <Route exact path={["/", "/books"]}>
-            <Books />
-          </Route>
-          <Route exact path="/books/:id">
-            <Detail />
-          </Route>
-          <Route>
-            <NoMatch />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <div className="App">
+          <Continents />
+          
+    </div>
   );
 }
 
