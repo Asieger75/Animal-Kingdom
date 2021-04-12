a49df5c0a21cfec3c0c40f8da689ec891dee20e8
antonneturner <71462708+antonneturner@users.noreply.github.com>
2021-04-12 17:33:50 -0400

    Update Asia.js
    fix route

--- client/src/components/Map/Asia/Asia.js
+++ client/src/components/Map/Asia/Asia.js
@@ -7,7 +7,7 @@ class Asia extends Component {
       };
   
       getData = () => {
-        fetch(`http://localhost:3000/api/animals`)
+        fetch(`/api/animals`)
           .then(response => response.json())
           .then(data => {
             const { query } = this.state;
